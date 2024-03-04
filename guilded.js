27bfcb691a45264cca56a3ecbe3c1fd6c8942412
User0 <104380885+user0-tb@users.noreply.github.com>
2024-03-04 14:32:53 +0100

    Update guilded.js

--- guilded.js
+++ guilded.js
@@ -1,1 +1,12 @@
-//test
+const { Client } = require("guilded.js");
+// import { Client } from "guilded.js";
+const client = new Client({ token: "gapi_1smLYF0mY4m8g0gum1e/SzHMvZv+nsrlpzFPLvQgdIDZZlzkqCquHRL0pbXc4Rdw7MtKDkG5WJLgXFuY6L8fKg==" });
+
+client.on("ready", () => console.log(`Bot is successfully logged in`));
+client.on("messageCreated", (message) => {
+    if (message.content === "test") {
+        return message.reply("test indeed");
+    }
+});
+
+client.login();
